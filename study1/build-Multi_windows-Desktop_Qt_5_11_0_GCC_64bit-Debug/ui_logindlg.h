c8f2dd1d016c14b294c12d84fa84f0fce9be72dc
wuhaostudy1992 <wuhaostudy1992@gmail.com>
2018-02-14 00:44:41 -0800

    update

---
+++ study1/build-Multi_windows-Desktop_Qt_5_11_0_GCC_64bit-Debug/ui_logindlg.h
@@ -0,0 +1,53 @@
+/********************************************************************************
+** Form generated from reading UI file 'logindlg.ui'
+**
+** Created by: Qt User Interface Compiler version 5.11.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_LOGINDLG_H
+#define UI_LOGINDLG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QPushButton>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_LoginDlg
+{
+public:
+    QPushButton *pushButton;
+
+    void setupUi(QDialog *LoginDlg)
+    {
+        if (LoginDlg->objectName().isEmpty())
+            LoginDlg->setObjectName(QStringLiteral("LoginDlg"));
+        LoginDlg->resize(400, 300);
+        pushButton = new QPushButton(LoginDlg);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(160, 110, 89, 25));
+
+        retranslateUi(LoginDlg);
+        QObject::connect(pushButton, SIGNAL(clicked()), LoginDlg, SLOT(accept()));
+
+        QMetaObject::connectSlotsByName(LoginDlg);
+    } // setupUi
+
+    void retranslateUi(QDialog *LoginDlg)
+    {
+        LoginDlg->setWindowTitle(QApplication::translate("LoginDlg", "Dialog", nullptr));
+        pushButton->setText(QApplication::translate("LoginDlg", "PushButton", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class LoginDlg: public Ui_LoginDlg {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_LOGINDLG_H
